b696bf6e404be6ab36a0463973343f0dbc7b0adb
Amit Chawla <acamit84@gmail.com>
2018-07-12 16:59:06 +0530

    Fixed length columns

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ActiveReportsDemo
+1. Create following folder in c drive 
+    code>report
+
+Max no of columns shown are 533 with fixed length. 
+If the columns are allowed to expand to natural length number of columns displayed decreases.
+
+Branch - master - variable length columns
+Branch - dev - fixed length columns
--- Reports/CustomReport.cs
+++ Reports/CustomReport.cs
@@ -1,7 +1,5 @@
 ﻿using GrapeCity.ActiveReports.Document.Section;
-
 using GrapeCity.ActiveReports.SectionReportModel;
-using System;
 using System.Collections;
 using System.Data;
 using System.Drawing;
@@ -13,13 +11,10 @@ namespace Reports
     /// </summary>
     public partial class CustomReport : GrapeCity.ActiveReports.SectionReport
     {
-        private DataTable dtSummary;
         private DataSet _dsSource;
         public string RptFileName = "";
-
-
         DataTable dtGLID;
-        private string strUserPrefCols = "";
+
         public CustomReport()
         {
             InitializeComponent();
@@ -37,137 +32,96 @@ namespace Reports
             DataView dvMaster;
 
             dvMaster = dtMaster.DefaultView;
-
-            int iUserPrefColStart = 4;
-            int iUserPrefColEnd = 8;
-
-            int iTotalUserSelectedCols = 0;
-            int iTotalsColStart = 0;
-
-            if (strUserPrefCols.Trim() != "")
-                iTotalUserSelectedCols = strUserPrefCols.Split(',').Length;
-
-            iTotalsColStart = 3 + iTotalUserSelectedCols;
-            iUserPrefColStart = 5;
-            iUserPrefColEnd = 12;
-            iTotalsColStart = 5 + iTotalUserSelectedCols;
-
-
-
             float fltTotalWidth = 0f;
 
             SizeF ControlSize;
-
-
             Page objPage = new Page();
             objPage.Units = Units.Inches;
             objPage.Font = new Font("Arial", 10, FontStyle.Regular);
 
             foreach (DataColumn dc in dtMaster.Columns)
             {
-                if ((dc.Ordinal >= iUserPrefColStart) && (dc.Ordinal <= iUserPrefColEnd))
-                {
-                    string strTempColName = dc.ColumnName.Trim().Replace("_cur", "").Replace("_num", "").Replace("_str", "").Replace("_dat", "");
-                    if (strUserPrefCols.IndexOf(strTempColName) < 0)
-                    {
-                        continue;
-                    }
-                }
-
                 Label lblHeader = new Label();
-                Label lblHeader2GL_ID = new Label();
+                Label Header2 = new Label();
                 TextBox tbData = new TextBox();
                 TextBox tbTotal = new TextBox();
 
-                if (dc.ColumnName.ToLower() != "pkid")
-                {
+                lblHeader.Text = dc.ColumnName.Trim();
+                Header2.Text = "Column Heading Name- Just making it larger than 15 to test the things.";
 
-
-                    lblHeader.Text = dc.ColumnName.Trim();
-                    lblHeader2GL_ID.Text = "Column Heading Name- Just making it larger than 15 to test the things.";
-
-                    ControlSize = objPage.MeasureText(dc.ColumnName);
+                ControlSize = objPage.MeasureText(dc.ColumnName);
+                if (ControlSize.Width < 1.5f)
                     ControlSize.Width = 1.5f;
 
-                    lblHeader.Width = ControlSize.Width;
-                    lblHeader2GL_ID.Width = ControlSize.Width;
-                    tbData.Width = ControlSize.Width;
-                    tbTotal.Width = ControlSize.Width;
+                lblHeader.Width = ControlSize.Width;
+                Header2.Width = ControlSize.Width;
+                tbData.Width = ControlSize.Width;
+                tbTotal.Width = ControlSize.Width;
 
-                    lblHeader.Border.BottomStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
-                    lblHeader.Border.LeftStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
-                    lblHeader.Border.RightStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
-                    lblHeader.Border.TopStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
-                    lblHeader.Style = "font-weight: bold; text-align: center; background-color: LightGrey";
-                    lblHeader.WrapMode = WrapMode.WordWrap;
-                    lblHeader.Height = 0.78f;
-
-
-                    lblHeader2GL_ID.Border.BottomStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
-                    lblHeader2GL_ID.Border.LeftStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
-                    lblHeader2GL_ID.Border.RightStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
-                    lblHeader2GL_ID.Border.TopStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
-                    lblHeader2GL_ID.Style = "font-weight: bold; text-align: center; background-color: LightGrey";
-                    lblHeader2GL_ID.WrapMode = WrapMode.WordWrap;
-                    lblHeader2GL_ID.Height = 0.78f;
+                lblHeader.Border.BottomStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
+                lblHeader.Border.LeftStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
+                lblHeader.Border.RightStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
+                lblHeader.Border.TopStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
+                lblHeader.Style = "font-weight: bold; text-align: center; background-color: LightGrey";
+                lblHeader.WrapMode = WrapMode.WordWrap;
+                lblHeader.Height = 0.78f;
 
 
-                    tbTotal.Border.BottomStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
-                    tbTotal.Border.LeftStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
-                    tbTotal.Border.RightStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
-                    tbTotal.Border.TopStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
-                    tbTotal.Style = "font-weight: bold; text-align: right; background-color: LightGrey";
+                Header2.Border.BottomStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
+                Header2.Border.LeftStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
+                Header2.Border.RightStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
+                Header2.Border.TopStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
+                Header2.Style = "font-weight: bold; text-align: center; background-color: LightGrey";
+                Header2.WrapMode = WrapMode.WordWrap;
+                Header2.Height = 0.78f;
 
-                    lblHeader.Font = new Font("Arial", 10, FontStyle.Bold);
-                    lblHeader2GL_ID.Font = new Font("Arial", 10, FontStyle.Italic);
-                    tbData.Font = new Font("Arial", 10, FontStyle.Regular);
-                    tbTotal.Font = new Font("Arial", 10, FontStyle.Bold);
 
-                    if (intCnt == 1)
-                    {
-                        lblHeader.Location = new PointF(.21f, .08f);
-                        lblHeader2GL_ID.Location = new PointF(.21f, lblHeader.Height);
-                        fltLastControlXPosition = .21f + ControlSize.Width;
-                        tbData.Location = new PointF(.21f, .06f);
-                        fltTotalWidth = ControlSize.Width + .21f;
-                    }
-                    else
-                    {
-                        lblHeader.Location = new PointF(fltLastControlXPosition, .08f);
-                        lblHeader2GL_ID.Location = new PointF(fltLastControlXPosition, lblHeader.Height);
-                        tbData.Location = new PointF(fltLastControlXPosition, .06f);
-                        tbTotal.Location = new PointF(fltLastControlXPosition, .06f);
-                        fltLastControlXPosition = lblHeader.Location.X + ControlSize.Width;
+                tbTotal.Border.BottomStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
+                tbTotal.Border.LeftStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
+                tbTotal.Border.RightStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
+                tbTotal.Border.TopStyle = GrapeCity.ActiveReports.BorderLineStyle.Solid;
+                tbTotal.Style = "font-weight: bold; text-align: right; background-color: LightGrey";
 
-                    }
+                lblHeader.Font = new Font("Arial", 10, FontStyle.Bold);
+                Header2.Font = new Font("Arial", 10, FontStyle.Italic);
+                tbData.Font = new Font("Arial", 10, FontStyle.Regular);
+                tbTotal.Font = new Font("Arial", 10, FontStyle.Bold);
 
-                    string strJ = dc.ColumnName.Trim();
-                    try
-                    {
-                        strJ = dc.ColumnName.Trim().Substring(dc.ColumnName.Trim().Length - 4, 4);
-                    }
-                    catch (Exception)
-                    {
-                        strJ = "_cur";
-                    }
-
-                    fltTotalWidth += ControlSize.Width;
-
-                    tbData.DataField = dc.ColumnName;
-                    tbTotal.DataField = dc.ColumnName;
-
-                    pageHeader.Controls.Add(lblHeader);
-                    pageHeader.Controls.Add(lblHeader2GL_ID);
-
-                    detail.Controls.Add(tbData);
-
-                    tbTotal.SummaryFunc = SummaryFunc.Sum;
-                    tbTotal.SummaryRunning = SummaryRunning.All;
-                    tbTotal.SummaryType = SummaryType.GrandTotal;
-
-                    tbTotal.OutputFormat = "#,##0.00";
-                    intCnt++;
+                if (intCnt == 1)
+                {
+                    lblHeader.Location = new PointF(.21f, .08f);
+                    Header2.Location = new PointF(.21f, lblHeader.Height);
+                    fltLastControlXPosition = .21f + ControlSize.Width;
+                    tbData.Location = new PointF(.21f, .06f);
+                    fltTotalWidth = ControlSize.Width + .21f;
                 }
+                else
+                {
+                    lblHeader.Location = new PointF(fltLastControlXPosition, .08f);
+                    Header2.Location = new PointF(fltLastControlXPosition, lblHeader.Height);
+                    tbData.Location = new PointF(fltLastControlXPosition, .06f);
+                    tbTotal.Location = new PointF(fltLastControlXPosition, .06f);
+                    fltLastControlXPosition = lblHeader.Location.X + ControlSize.Width;
+
+                }
+
+                fltTotalWidth += ControlSize.Width;
+
+                tbData.DataField = dc.ColumnName;
+                tbTotal.DataField = dc.ColumnName;
+
+                pageHeader.Controls.Add(lblHeader);
+                pageHeader.Controls.Add(Header2);
+
+                detail.Controls.Add(tbData);
+
+                tbTotal.SummaryFunc = SummaryFunc.Sum;
+                tbTotal.SummaryRunning = SummaryRunning.All;
+                tbTotal.SummaryType = SummaryType.GrandTotal;
+
+                tbTotal.OutputFormat = "#,##0.00";
+                intCnt++;
+
             }
             //this.PageSettings.PaperKind = System.Drawing.Printing.PaperKind.Custom;
             this.PageSettings.PaperWidth = fltTotalWidth + 5.0f;
@@ -186,7 +140,7 @@ namespace Reports
             {
                 dt.Columns.Add("This is a column name greater than 15 charcaters. " + i);
             }
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i < 10; i++)
             {
                 DataRow dr = dt.NewRow();
                 for (int j = 0; j < 600; j++)
